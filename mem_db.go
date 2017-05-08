354990cb4f5e91b79c498d2232cac88d173c233a
haoxiang <linhaoxiang32@gmail.com>
2017-05-08 11:39:10 +0800

    update memory db code

--- mem_db.go
+++ mem_db.go
@@ -10,7 +10,6 @@ import (
 )
 
 const tMaxHeight = 12
-<<<<<<< HEAD
 
 type mNode struct {
 	timeKey int64
@@ -30,27 +29,6 @@ func (p *mNode) setNext(n int, x *mNode) {
 	atomic.StorePointer(&p.next[n], unsafe.Pointer(x))
 }
 
-=======
-
-type mNode struct {
-	timeKey int64
-	value   map[string]float64
-	next    []unsafe.Pointer
-}
-
-func newNode(timeKey int64, value map[string]float64, height int32) *mNode {
-	return &mNode{timeKey, value, make([]unsafe.Pointer, height)}
-}
-
-func (p *mNode) getNext(n int) *mNode {
-	return (*mNode)(atomic.LoadPointer(&p.next[n]))
-}
-
-func (p *mNode) setNext(n int, x *mNode) {
-	atomic.StorePointer(&p.next[n], unsafe.Pointer(x))
-}
-
->>>>>>> fb1833465f17851ab17fdf333ebff3205a2ab5ca
 func (p *mNode) getNext_NB(n int) *mNode {
 	return (*mNode)(p.next[n])
 }
@@ -123,7 +101,6 @@ func (p *DB) Remove(key int64) {
 	x, exact := p.findGE_NB(key, true)
 	if !exact {
 		return
-<<<<<<< HEAD
 	}
 
 	h := len(x.next)
@@ -131,15 +108,6 @@ func (p *DB) Remove(key int64) {
 		n.setNext(i, n.getNext_NB(i).getNext_NB(i))
 	}
 
-=======
-	}
-
-	h := len(x.next)
-	for i, n := range p.prev[:h] {
-		n.setNext(i, n.getNext_NB(i).getNext_NB(i))
-	}
-
->>>>>>> fb1833465f17851ab17fdf333ebff3205a2ab5ca
 	// TODO
 	//p.kvSize -=
 	//atomic.AddInt64(&p.kvSize, -int64(x.timeKey + len(x.value)))
@@ -156,7 +124,6 @@ func (p *DB) Get(timeKey int64, key string) (float64, error) {
 		value, ok := x.value[key]
 		if ok {
 			return value, nil
-<<<<<<< HEAD
 		}
 		//return x.value, nil
 	}
@@ -218,12 +185,8 @@ func (p *DB) findGE(key int64, prev bool) (*mNode, bool) {
 				return next, cmp == 0
 			}
 			h--
-=======
->>>>>>> fb1833465f17851ab17fdf333ebff3205a2ab5ca
 		}
-		//return x.value, nil
 	}
-<<<<<<< HEAD
 	return nil, false
 }
 
@@ -261,48 +224,6 @@ func (p *DB) findGE_NB(key int64, prev bool) (*mNode, bool) {
 	h := int(p.maxHeight) - 1
 	for {
 		next := x.getNext_NB(h)
-=======
-	return -1.0, errors.New("")
-}
-
-func (p *DB) GetRange(startTime int64, endTime int64, key string) ([]int64, []float64, error) {
-	//var res []float64
-	resKey, resValue, ok := p.findRange(startTime, endTime, key)
-	if ok {
-		return resKey, resValue, nil
-	}
-	return resKey, resValue, nil
-}
-
-func (p *DB) Find(key int64) (int64, map[string]float64, error) {
-	if x, _ := p.findGE(key, false); x != nil {
-		return x.timeKey, x.value, nil
-	}
-	return -1, nil, errors.New("")
-}
-
-// NewIterator create a new iterator over the database content.
-func (p *DB) NewIterator() *Iterator {
-	return &Iterator{p: p}
-}
-
-// Size return sum of key/value size.
-func (p *DB) Size() int {
-	return int(atomic.LoadInt64(&p.kvSize))
-}
-
-// Len return the number of entries in the database.
-func (p *DB) Len() int {
-	return int(atomic.LoadInt32(&p.n))
-}
-
-// Must hold RW-lock if prev == true, as it use shared prevNode slice.
-func (p *DB) findGE(key int64, prev bool) (*mNode, bool) {
-	x := p.head
-	h := int(atomic.LoadInt32(&p.maxHeight)) - 1
-	for {
-		next := x.getNext(h)
->>>>>>> fb1833465f17851ab17fdf333ebff3205a2ab5ca
 		var cmp int64
 		cmp = 1
 		if next != nil {
@@ -316,24 +237,16 @@ func (p *DB) findGE(key int64, prev bool) (*mNode, bool) {
 				p.prev[h] = x
 			} else if cmp == 0 {
 				return next, true
-<<<<<<< HEAD
 			}
 			if h == 0 {
 				return next, cmp == 0
 			}
-=======
-			}
-			if h == 0 {
-				return next, cmp == 0
-			}
->>>>>>> fb1833465f17851ab17fdf333ebff3205a2ab5ca
 			h--
 		}
 	}
 	return nil, false
 }
 
-<<<<<<< HEAD
 func (p *DB) findLT(timeKey int64) *mNode {
 	x := p.head
 	h := int(atomic.LoadInt32(&p.maxHeight)) - 1
@@ -447,110 +360,4 @@ func (i *Iterator) Value() map[string]float64 {
 
 func (i *Iterator) Error() error {
 	return nil
-=======
-// Must hold RW-lock if prev == true, as it use shared prevNode slice.
-func (p *DB) findRange(startKey int64, endKey int64, key string) ([]int64, []float64, bool) {
-	x := p.head
-	h := 0
-	var resValue []float64
-	var resKey [] int64
-	for {
-		next := x.getNext(h)
-		if next == nil {
-			return resKey, resValue, true
-		} else {
-			if startKey > next.timeKey {
-				return resKey, resValue, true
-			} else if endKey < next.timeKey {
-				x = next
-			} else if startKey <= next.timeKey && next.timeKey <= endKey {
-				// Keep searching in this list
-				resValue = append(resValue, next.value[key])
-				resKey = append(resKey, next.timeKey)
-				x = next
-			}
-		}
-	}
-	return resKey, resValue, false
-}
-
-
-
-// Must hold RW-lock if prev == true, as it use shared prevNode slice.
-func (p *DB) findGE_NB(key int64, prev bool) (*mNode, bool) {
-	x := p.head
-	h := int(p.maxHeight) - 1
-	for {
-		next := x.getNext_NB(h)
-		var cmp int64
-		cmp = 1
-		if next != nil {
-			cmp = key - next.timeKey
-		}
-		if cmp < 0 {
-			// Keep searching in this list
-			x = next
-		} else {
-			if prev {
-				p.prev[h] = x
-			} else if cmp == 0 {
-				return next, true
-			}
-			if h == 0 {
-				return next, cmp == 0
-			}
-			h--
-		}
-	}
-	return nil, false
-}
-
-func (p *DB) findLT(timeKey int64) *mNode {
-	x := p.head
-	h := int(atomic.LoadInt32(&p.maxHeight)) - 1
-	for {
-		next := x.getNext(h)
-		if next == nil || (timeKey - next.timeKey) >= 0 {
-			if h == 0 {
-				if x == p.head {
-					return nil
-				}
-				return x
-			}
-			h--
-		} else {
-			x = next
-		}
-	}
-	return nil
-}
-
-func (p *DB) findLast() *mNode {
-	x := p.head
-	h := int(atomic.LoadInt32(&p.maxHeight)) - 1
-	for {
-		next := x.getNext(h)
-		if next == nil {
-			if h == 0 {
-				if x == p.head {
-					return nil
-				}
-				return x
-			}
-			h--
-		} else {
-			x = next
-		}
-	}
-	return nil
-}
-
-func (p *DB) randHeight() (h int32) {
-	const branching = 4
-	h = 1
-	for h < tMaxHeight && p.rnd.Int() % branching == 0 {
-		h++
-	}
-	return
->>>>>>> fb1833465f17851ab17fdf333ebff3205a2ab5ca
 }
